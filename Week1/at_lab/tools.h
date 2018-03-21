7aab85cd7994451463ca5e2cbab53db5a84e8e27
Cindy <37494574+CindyChenGitHub@users.noreply.github.com>
2018-03-21 01:35:50 +0000

    Update tools.h

--- Week1/at_lab/tools.h
+++ Week1/at_lab/tools.h
@@ -1,6 +1,12 @@
+/********************************************************************** 
+// OOP244 Workshop 1: Compiling modular source code 
+// File tools.h 
+// Version 1.0 
+// Date 2018/01/10 
+// Author Cindy Chen 
+/**********************************************************************/ 
+
 // Displays the user interface menu
 int menu();
-
 // Performs a fool-proof integer entry
 int getInt(int min, int max);
-
