257c511a467ec7b24ffc801204a629566bf5ddb9
CindyChen <cindy@cindychendemacbook-air.local>
2018-06-16 22:18:30 -0400

    push

---
+++ Week 8/Hello world/Hello world/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hello world
+//
+//  Created by CindyChen on 2018/6/16.
+//  Copyright © 2018 Seneca College. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
