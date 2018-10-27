599c67214e36aa8dd60fea18ec32e35262dc73c5
Cindy <blueapple.phoebe@gmail.com>
2018-10-27 00:15:22 -0400

    v2.2

--- wk1/PrintInt.java
+++ wk1/PrintInt.java
@@ -1,21 +1,32 @@
 /**
- * This program reads an integer and prints it in binary, octal, hexadecimal, and hexadecimal floating-point of the reciprocal
+ * This program reads an integer and prints it 
+ * in binary, octal, hexadecimal, and hexadecimal floating-point of the reciprocal
  * @author Cindy
- * @version 2.0 2018-10-26
+ * @version 2.0 
+ * @Date 2018-10-26
+ * Purpose : JAC444 Assignment 1
  */
 package jac444.wk1;
 import java.util.Scanner;
+
+/**
+ * Class Name: PrintInt
+ */
 public class PrintInt{
-    public static void main(String[] args) {
-    	// read an integer number
-    	int num = 0;
+	/**
+	 * Read an integer
+	 * @param void
+	 * @return Integer
+	 */
+	public static Integer readInteger() {
+		Integer readInt = 0;
     	boolean again = false;
     	do {
     		@SuppressWarnings("resource")
     		Scanner in = new Scanner(System.in);
 	    	System.out.print("Please input a integer number: ");
 	    	if(in.hasNextInt()) {
-	    		num = in.nextInt();
+	    		readInt = in.nextInt();
 	    		again = false;
 	    	}
 	    	else {
@@ -23,16 +34,52 @@ public class PrintInt{
 	    		again = true;
 	    	}
     	}while(again);
-    	// print
-    	System.out.printf("Banary: %s\n" , Integer.toBinaryString(num)); //show binary
-        System.out.printf("Octal: %o\n" ,num); //show octal
-        System.out.printf("Hexadecimal: %x\n" ,num); //show hex
-        System.out.printf("Reciprocal: %a\n", (float)1.0/num); //show Reciprocal
+    	return readInt;
+	}
+	/**
+	 * Print in binary, octal, hexadecimal, and hexadecimal floating-point of the reciprocal 
+	 * @param Integer
+	 * @return void
+	 */
+	public static void formatPrint(Integer inInt) {
+		System.out.printf("Your Integer in Binary is: %s\n" , Integer.toBinaryString(inInt));
+        System.out.printf("Your Integer in Octal is: %s\n" ,Integer.toOctalString(inInt));
+        System.out.printf("Your Integer in Hexadecimal is: %s\n" ,Integer.toHexString(inInt));
+        System.out.printf("Its Hex-float-point reciprocal is: %a\n", (float)1.0/inInt);
+	}
+	/**
+	 * pause for dramatic appearance 
+	 * @param void
+	 * @return void
+	 */
+	public static void pause(String in_read) {
+		System.out.println("Your input is : " + in_read + "\n~Press Any Key to Convert...");
+		Scanner pause = new Scanner(System.in);
+		pause.nextLine();
+		pause.close();
+	}
+	/**
+	 * Main will use java.util.Scanner for user input.
+	 * @see #readInteger()
+	 * @see #pause(String)
+	 * @see #formatPrint(Integer)
+	 * @param args Not Used.
+	 */
+    public static void main(String[] args) {
+    	// read an integer number
+    	Integer readInt = readInteger();
+    	// pause
+    	pause(Integer.toString(readInt));
+    	// Format printing
+    	formatPrint(readInt);
     }
 }
-// Output:
-// Please input a number: 76
-// Banary: 1001100
-// Octal: 114
-// Hexadecimal: 4c
-// Reciprocal: 0x1.af286cp-7
+//Output:
+//Please input a integer number: 76
+//Your input is : 76
+//~Press Any Key to Convert...
+//
+//Your Integer in Binary is: 1001100
+//Your Integer in Octal is: 114
+//Your Integer in Hexadecimal is: 4c
+//Its Hex-float-point reciprocal is: 0x1.af286cp-7
